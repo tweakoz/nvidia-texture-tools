3e362bb32df270f093f4d1e923e48216481c38e6
Michael Mayers <tweakoz@users.noreply.github.com>
2021-04-09 04:19:00 -0600

    Update nvthread.cpp

--- src/nvthread/nvthread.cpp
+++ src/nvthread/nvthread.cpp
@@ -8,7 +8,7 @@
 #include "Win32.h"
 #elif NV_OS_UNIX
 #include <sys/types.h>
-#include <sys/sysctl.h>
+//#include <sys/sysctl.h> // deprecated header
 #include <unistd.h>
 #elif NV_OS_DARWIN
 #import <stdio.h>
